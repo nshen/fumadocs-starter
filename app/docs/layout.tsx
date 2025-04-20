9d7844939448e0e07879918853fc66bb240f65f4
Xin Chen <nshen121@gmail.com>
2025-04-20 17:02:57 +0800

    fix: move rootprovider to docs/

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,12 +1,22 @@
+import { RootProvider } from 'fumadocs-ui/provider';
 import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
- 
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
-      {children}
-    </DocsLayout>
+
+    <RootProvider
+    // default dark mode
+    // theme={{
+    //   defaultTheme: 'dark',
+    //   enableSystem: true
+    // }}
+    >
+      <DocsLayout tree={source.pageTree} {...baseOptions}>
+        {children}
+      </DocsLayout>
+    </RootProvider>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { RootProvider } from 'fumadocs-ui/provider';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -26,13 +25,7 @@ export default function RootLayout({
   return (
     <html>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`} >
-        <RootProvider
-          // default dark mode
-          // theme={{
-          //   defaultTheme: 'dark',
-          //   enableSystem: true
-          // }}
-        >{children}</RootProvider>
+        {children}
       </body>
     </html>
   );
